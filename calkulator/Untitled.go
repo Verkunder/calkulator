7048d77dadc0b4979498e7321e5ac82bdb21af60
Verkunder <Verkunder@mail.ru>
2018-08-19 14:07:01 +0300

    Hello

--- calkulator/Untitled.go
+++ calkulator/Untitled.go
@@ -1,48 +1,17 @@
 package main
 
-import ( 
-"fmt" )
+import (
+	"fmt"
+"log"
+"net/http")
 
-func main () {
-	var a int
-	var b int
-	sum:=0
-	var Oper string
-	Otv:="Да"
-	fmt.Println("Введите число а")
-	fmt.Scanf("%d \n", &a)
-	fmt.Println("Введите число b")
-	fmt.Scanf("%d \n", &b)
-	fmt.Println("Введите операцию")
-	fmt.Scanf("%s \n", &Oper)
-	switch Oper {
- case "+": sum=sum+a+b 
- case "-": sum=sum+a-b 
- case "/": sum=sum+a/b 
- case "*": sum=sum+a*b 
-default: fmt.Println("Введите корректную операцию")
-		 fmt.Scanf("%s \n", &Oper) 
- }
- fmt.Println("Итог",sum)
- for Otv=="Да"{
- fmt.Println("Продолжить работу?(Да или Нет)")
- fmt.Scanf("%s \n", &Otv)
- switch Otv {
- case "Да": fmt.Println("Введите число")
- fmt.Scanf("%d \n", &b)
- fmt.Println("Введите операцию")
- fmt.Scanf("%s \n", &Oper)
- switch Oper {
-	case "+": sum=sum+b 
-	case "-": sum=sum-b 
-	case "/": sum=sum/b 
-	case "*": sum=sum*b 
-   default: fmt.Println("Введите корректную операцию")
-			fmt.Scanf("%s \n", &Oper) 
-	}
- case "Нет": fmt.Println("Спасибо")
- }
-	fmt.Println("Итог=",sum)
-}
+func handler(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w, "Hello World %s!", r.URL.Path[1:])
+	
 }
 
+func main()  {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080",nil))
+	
+}
